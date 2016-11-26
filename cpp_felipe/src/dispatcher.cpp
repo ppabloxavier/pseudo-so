f9e02564202333d855fbdfc118872df5ed475064
PedroPlima <ppslima@gmail.com>
2016-11-26 13:11:50 -0200

    Minor change in gitignore (ignoring binaries) and reading of the processes file in the dispatcher. Minor changes in make file to run from 'root' folder. (make run)

--- cpp_felipe/src/dispatcher.cpp
+++ cpp_felipe/src/dispatcher.cpp
@@ -1,8 +1,10 @@
+#include <fstream>
+#include <iostream>
+#include <sstream>
 #include <stdio.h>
 #include <stdlib.h>
-#include <vector>
 #include <string>
-#include <iostream>
+#include <vector>
 
 #include "../include/Memory_mng.hpp"
 #include "../include/Process_mng.hpp"
@@ -31,12 +33,55 @@ void print_info(Process process) {
 // Lê o arquivo de processos e os adiona a um vetor.
 //
 // Parâmetros:
-//	file_name: nome do arquivo com as informações dos processos.
+//	fileName: nome do arquivo com as informações dos processos.
 //	*processes: ponteiro para o vetor no qual os processos serão salvos.
 // Retorno:
 //	void
-void read_process_file(string file_name, std::vector<Process> *processes) {
+void read_processes_file(string fileName, std::vector<Process> *processes) {
+	ifstream inputFile;
+    int i;
+    Process process;
+	vector<int> processData; /* param order
+						1- init time
+						2- priority
+						3- cpuTime
+						4- memBlocks
+						5- printerCode
+						6- scannerCode
+						7- modemCode
+						8- diskCode
+						 */
 
+    // open file for reading
+    inputFile.open(fileName.c_str());
+
+	if(inputFile.is_open()) {
+ 		// read file until the end (if sucessfully opened)
+	    string str;
+	    
+		while (getline(inputFile, str)) {
+			cout << "Processo atual: [" << str << "]" << endl;
+			stringstream ss(str);
+			while(ss >> i) {
+				processData.push_back(i);
+				if (ss.peek() == ',') {
+					ss.ignore();
+				}
+			}
+			ss.str("");
+			ss.clear();
+
+			for (i=0; i < processData.size(); i++) {
+				cout << processData.at(i) << ' ';
+			}
+			cout << endl;
+		}
+
+		// close file
+	    inputFile.close();
+ 	} else {
+ 		cout << "Error opening file." << endl;
+ 	}
 }
 
 // Inicia cada processo do vetor de processos.
@@ -72,12 +117,11 @@ Na crição do processo, o dipatcher exibe as seguintes mensagens:
 
 */
 
-	string file_name;
+	string fileName = argv[1];
 	std::vector<Process> processes;
 
-	// leitura do arquivo que contém os processos a serem criados. le tudo de uma vez e armazena num vetor de vetores de inteiros
-	// ou vai fazendo um por um?
-	read_process_file(file_name, &processes);
+	// leitura do arquivo que contém os processos a serem criados. le tudo de uma vez
+	read_processes_file(fileName, &processes);
 
 	// fecha-se o arquivo (depende de como será o input)
 

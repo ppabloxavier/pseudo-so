8bd3bbd001ff4ed2f6976f61caeb892db31c6edc
PedroPlima <ppslima@gmail.com>
2016-11-27 19:50:10 -0200

    Memory manager almost done. Problema with variable scope.

--- src/Memory_mng.cpp
+++ src/Memory_mng.cpp
@@ -1,29 +1,59 @@
-#include "../include/Memory_mng.hpp"
+#include "Memory_mng.hpp"
 
 Memory_mng::Memory_mng() {
 
 }
 
-void Memory_mng::allocateMemory(int pid, int offset) {
-
-}
-
-int Memory_mng::getProcessOffset(int pid) {
-	std::map<int,int>::iterator it = processOffset.find(pid);
-	if (it != processOffset.end()) {
-		return it->second;
+void allocateMemory(Process process) {
+	bool allocationOcurred = false;
+	int i, j, begin, end, freeSize, offset;
+	if(process.priority <= 0) {
+		begin = REAL_TIME_START;
+		end = REAL_TIME_END;
 	} else {
-		std::cout << "Process not allocated" << std::endl;
-		return -1;
+		begin = USER_START;
+		end = USER_END;
 	}
+
+	for(i = begin; i <= end; i=i) {
+		// se memoria ocupada, pule até achar um espaço vazio
+		if(ramMemory.at(i)) {
+			while(ramMemory.at(i)) {
+				i++;
+			}
+		} else {
+			// se espaço vazio, conte quanto espaço vazio.
+			offset = i;
+			freeSize = 0;
+			while(!ramMemory.at(i) && i<= end) {
+				freeSize++;
+				i++;
+			}
+
+			// serviu?
+			if (freeSize >= process.memory_blocks) {
+				process.memory_offset = offset;
+				for (j=offset; j<i; j++) {
+					ramMemory.at(j) = true;
+				}
+				allocationOcurred = true;
+				break;
+			} else {
+				// nao serviu? pule este espaço pequeno
+				i = i + freeSize;
+			}
+		}
+	}
+
+	if(!allocationOcurred) {
+		std::cout << "Not enough memory for process with pid: " << process.pid << std::endl;
+	}
+
 }
 
-int Memory_mng::getSizeOfFreeMemory(int offset) {
-	int i, size = 0;
-
-	i = offset;
-	while(!ramMemory[i] || i <= REAL_TIME_END || i <= USER_END) {
-		size++;
+void deallocateMemory(Process process) {
+	int i;
+	for(i = process.memory_offset; i <= (process.memory_offset + process.memory_blocks); i++) {
+		ramMemory.at(i) = false;
 	}
-	return size;
 }
